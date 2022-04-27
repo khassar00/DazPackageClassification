cce955b893203ca88e1cd60947771f453bed9ebb
khassar <khassar@qq.com>
2022-04-27 13:21:14 +0800

    test Github Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
+import org.gradle.crypto.checksum.Checksum
+import org.gradle.internal.impldep.org.bouncycastle.asn1.bsi.BSIObjectIdentifiers.*
+import org.gradle.nativeplatform.SharedLibraryBinarySpec.TasksCollection
+import org.jetbrains.compose.ComposePlugin.Dependencies.desktop
 import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.compose") version "1.1.0"
+    id("org.gradle.crypto.checksum") version "1.4.0"
 }
 
 group = "me.khassar"
@@ -14,18 +20,32 @@ repositories {
     google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://jitpack.io")
+
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
+    implementation("com.github.komputing.khash:keccak:1.1.1")
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
-tasks.create("packageVersion"){
-    doLast{
+tasks.create("packageVersion") {
+    doLast {
         print(compose.desktop.application.nativeDistributions.packageVersion)
     }
+}
+
+tasks.create<Checksum>("checksum") {
+
+
+    inputFiles.setFrom(File("build/compose/binaries/main/msi"))
+    outputDirectory.set(File("build/compose/binaries/main/msi"))
+    checksumAlgorithm.set(Checksum.Algorithm.SHA256)
+    appendFileNameToChecksum.set(false)
+
+
 }
 
 compose.desktop {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,9 @@ pluginManagement {
         google()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven ("https://jitpack.io" )
     }
-    
+
 }
 rootProject.name = "DazPackageClassification"
 
